c2dc451d010269ff396327f0a3450d876866235f
alcubierre-drive <alcubierre-drive>
2024-07-12 22:49:25 +0200

    trying to make the acb function fast in a loop, but it seems like this is not really possible

--- test/use.c
+++ test/use.c
@@ -7,46 +7,61 @@
 #include <flint/arb.h>
 #include <flint/acb.h>
 #include <flint/acb_hypgeom.h>
+#include <unistd.h>
 
-static inline complex128_t bessel_I_acb( complex128_t nu_, complex128_t z_ ) {
+typedef struct {
     acb_t res;
-    acb_init(res);
-
     acb_t z, nu;
-    acb_init(z),
-    acb_init(nu);
-    acb_set_d_d( nu, creal(nu_), cimag(nu_) );
-    acb_set_d_d( z, creal(z_), cimag(z_) );
-    acb_hypgeom_bessel_i(res, nu, z, 48);
-    acb_clear(z),
-    acb_clear(nu);
+    FILE* f;
+    int fd;
+} bessel_I_acb_handle_t;
 
-    arb_t res_r, res_i;
-    arb_init( res_r );
-    arb_init( res_i );
-    acb_get_real( res_r, res );
-    acb_get_imag( res_i, res );
-    acb_clear(res);
-
-    char* r = arb_get_str( res_r, 16, ARB_STR_NO_RADIUS );
-    double rr = atof(r);
-    flint_free(r);
-    char* i = arb_get_str( res_i, 16, ARB_STR_NO_RADIUS );
-    double ii = atof(i);
-    flint_free(i);
-
-    arb_clear( res_r );
-    arb_clear( res_i );
-
-    return rr + I*ii;
+static inline bessel_I_acb_handle_t* bessel_I_acb_handle_init( void ) {
+    bessel_I_acb_handle_t* h = calloc(1, sizeof(bessel_I_acb_handle_t));
+    acb_init(h->res);
+    acb_init(h->z);
+    acb_init(h->nu);
+    h->f = tmpfile();
+    h->fd = fileno(h->f);
+    ftruncate(h->fd, 128);
+    return h;
 }
 
-static inline complex128_t bessel_I_ratio_flint( complex128_t nu, complex128_t z ) {
-    return bessel_I_acb(nu, z)/bessel_I_acb(nu-1.0, z);
+static inline void bessel_I_acb_handle_free( bessel_I_acb_handle_t* h) {
+    acb_clear(h->res);
+    acb_clear(h->z);
+    acb_clear(h->nu);
+    fclose(h->f);
+    free(h);
+}
+
+static inline complex128_t bessel_I_acb( bessel_I_acb_handle_t* h, complex128_t nu, complex128_t z ) {
+    complex128_t Cres;
+
+    acb_set_d_d( h->nu, creal(nu), cimag(nu) );
+    acb_set_d_d( h->z, creal(z), cimag(z) );
+    acb_hypgeom_bessel_i( h->res, h->nu, h->z, 48 );
+
+    rewind( h->f );
+    arb_fprintn( h->f, acb_realref(h->res), 16, ARB_STR_NO_RADIUS );
+    fputc(' ', h->f);
+    arb_fprintn( h->f, acb_imagref(h->res), 16, ARB_STR_NO_RADIUS );
+
+    rewind( h->f );
+    fscanf( h->f, "%le %le", &Cres, (double*)(&Cres)+1);
+
+    return Cres;
+}
+
+static inline complex128_t bessel_I_ratio_flint( bessel_I_acb_handle_t* h, complex128_t nu, complex128_t z ) {
+    return bessel_I_acb(h, nu, z)/bessel_I_acb(h, nu-1.0, z);
 }
 #endif // COMPARE_TO_FLINT
 
 int main() {
+#ifdef COMPARE_TO_FLINT
+    bessel_I_acb_handle_t* h = bessel_I_acb_handle_init();
+#endif
     for (double g=-1.0; g<=1.0+1.e-7; g+=1.e-2)
     for (double c=-1.0; c<=1.0+1.e-7; c+=1.e-2) {
         complex128_t nu = 1.0 + I * g,
@@ -54,10 +69,13 @@ int main() {
         complex128_t J2 = bessel_I_ratio( nu, z, NULL ),
                      J3 = bessel_I_ratio_iter( nu, z, 100 );
 #ifdef COMPARE_TO_FLINT
-        complex128_t J1 = bessel_I_ratio_flint( nu, z );
+        complex128_t J1 = bessel_I_ratio_flint( h, nu, z );
         printf( "%.5e %.5e %.5e %.5e %.5e %.5e\n", creal(J1), cimag(J1), creal(J2), cimag(J2), creal(J3), cimag(J3) );
 #else // COMPARE_TO_FLINT
         printf( "%.5e %.5e %.5e %.5e\n", creal(J2), cimag(J2), creal(J3), cimag(J3) );
 #endif // COMPARE_TO_FLINT
     }
+#ifdef COMPARE_TO_FLINT
+    bessel_I_acb_handle_free(h);
+#endif
 }
